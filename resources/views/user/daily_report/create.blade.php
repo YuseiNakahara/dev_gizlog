92baaaad3e59fbd77e8e6d4f0ef53192336a81a1
中原優成 <>
2019-05-20 17:28:22 +0900

    日報作成のFormタグ修正したのですが、もう一度修正版です

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -11,11 +11,11 @@
         <span class="help-block">{{ $errors->first('reporting_time') }}</span>
       </div>
       <div class="form-group {{ $errors->has('title')? 'has-error' : '' }}">
-      {!! Form::input('text', 'title', null, ['class' => 'form-control']) !!}
+      {!! Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
         <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
       <div class="form-group {{ $errors->has('contents')? 'has-error' : '' }}">
-        {!! Form::textarea('contents', null, ['rows' => 10,'cols' => 50], ['class' => 'form-comtrol']) !!}
+        {!! Form::textarea('contents', null, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
         <span class="help-block">{{ $errors->first('contents') }}</span>
       </div>
       {!! Form::submit('ADD', ['class' => 'btn btn-success pull-right']) !!}
