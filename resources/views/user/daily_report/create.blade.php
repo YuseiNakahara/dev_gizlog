a2d24bd73cb12e6331a28ae9ccf94195ca7ffb2d
中原優成 <>
2019-05-20 16:25:37 +0900

    日報作成のFormタグ書き忘れの修正

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -15,7 +15,7 @@
         <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
       <div class="form-group {{ $errors->has('contents')? 'has-error' : '' }}">
-        <textarea class="form-control" placeholder="Content" name="contents" cols="50" rows="10"></textarea>
+        {!! Form::textarea('contents', null, ['rows' => 10,'cols' => 50], ['class' => 'form-comtrol']) !!}
         <span class="help-block">{{ $errors->first('contents') }}</span>
       </div>
       {!! Form::submit('ADD', ['class' => 'btn btn-success pull-right']) !!}
